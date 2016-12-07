1b3acfcdecc8b65491e32eb6f6b7a88cb136c210
Issac O'Hara <issacohara@gmail.com>
2016-12-07 15:30:50 -0500

    Starting Files
    Starting Files

---
+++ routes/games.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+var Game = require('../models/game');
+
+//GET - Reading this Resource
+//POST - Creating this Resource
+//PUT - Updating this Resource
+//DELETE - Deleting this Resource
+
+module.exports = router;
